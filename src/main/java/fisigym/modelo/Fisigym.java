aa6ff5a8ffedbc1c381d81bd17df5db046a57e22
irishbe <iris.hanampa@unmsm.edu.pe>
2024-11-08 21:54:49 -0500

    Prueba de variables secretas

--- src/main/java/fisigym/modelo/Fisigym.java
+++ src/main/java/fisigym/modelo/Fisigym.java
@@ -2,7 +2,17 @@ package fisigym.modelo;
 
 public class Fisigym {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println("Hello World!");
+    }*/
+
+    public static void main(String[] args) {
+        String dbPassword = System.getenv("DB_PASSWORD");
+
+        if (dbPassword == null) {
+            System.out.println("La variable DB_PASSWORD no está configurada.");
+        } else {
+            System.out.println("La contraseña de la base de datos es: " + dbPassword);
+        }
     }
 }
