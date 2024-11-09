dd017c39604ad0f0224a2eaeb39a1094f7cc557e
irishbe <iris.hanampa@unmsm.edu.pe>
2024-11-08 23:30:29 -0500

    Prueba 2

--- src/main/java/fisigym/modelo/Fisigym.java
+++ src/main/java/fisigym/modelo/Fisigym.java
@@ -9,6 +9,14 @@ public class Fisigym {
             System.out.println("BIEN");
         } else {
             System.out.println("NOOOOOOOOOOOOOOOOOO");
+            String host = System.getenv("DB_HOST");
+            System.out.println(host);
+            System.out.println(System.getenv("DB_HOST"));
+            System.out.println(System.getenv("DB_NAME"));
+            System.out.println(System.getenv("DB_PASSWORD"));
+            System.out.println(System.getenv("DB_USER"));
+            System.out.println(System.getenv("DB_PORT"));
+            System.out.println(System.getenv("DB_URL"));
         }
     }
 }
