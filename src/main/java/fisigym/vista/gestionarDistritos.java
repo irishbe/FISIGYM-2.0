e66457eaf5cdd7c52e22bfd501902ef746caaf53
davguzm16 <guzman.davi16@gmail.com>
2024-11-14 16:42:06 -0500

    DAO, Vista y Controlador estabilizados 2 en main

--- src/main/java/fisigym/vista/gestionarDistritos.java
+++ src/main/java/fisigym/vista/gestionarDistritos.java
@@ -194,6 +194,12 @@ public class GestionarDistritos extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
